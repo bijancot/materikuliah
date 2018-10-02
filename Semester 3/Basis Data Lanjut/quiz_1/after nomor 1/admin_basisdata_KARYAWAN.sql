142a01a4ffd558466340403dbe32802e62b4bd40
bijancot <panjidia995@gmail.com>
2018-10-02 07:49:42 +0700

    add quiz BDL

---
+++ Semester 3/Basis Data Lanjut/quiz_1/after nomor 1/admin_basisdata_KARYAWAN.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
+--
+-- Host: 172.17.0.2    Database: admin_basisdata
+-- ------------------------------------------------------
+-- Server version	5.6.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `KARYAWAN`
+--
+
+LOCK TABLES `KARYAWAN` WRITE;
+/*!40000 ALTER TABLE `KARYAWAN` DISABLE KEYS */;
+INSERT INTO `KARYAWAN` VALUES ('KR002','TK07','','Ahmad','ahmad@gmail.com','81233445567'),('KR003','TK08','','Sarah','sarah@gmail.com','81233445568'),('KR004','TK09','','Akbar ','akbar@gmail.com','81233445569'),('KR005','TK10','','Agus','agus@gmail.com','81233445570'),('KR006','TK11','','Bowo','bowo@gmail.com','81233445571'),('KR007','TK12','','Sholeh','sholeh@gmail.co','81233445572'),('KR008','TK07','','Roy','Roy@gmail.com','81233445573'),('KR009','TK08','','Martin','Martin@gmail.co','81233445574'),('KR010','TK09','','Gading','Gading@gmail.co','81233445575'),('KR011','TK10','','Murni','Murni@gmail.com','81233445576'),('KR012','TK11','','Paijo','Paijo@gmail.com','81233445577'),('KR013','TK12','','Jhonson','Jhonson@gmail.c','81233445578'),('KR014','TK07','','Miya','Miya@gmail.com','81233445579'),('KR015','TK08','','Alucard','Alucard@gmail.c','81233445580'),('KR016','TK09','','Mulyani','Mulyani@gmail.c','81233445581'),('KR017','TK10','','Natalia','Natalia@gmail.c','81233445582'),('KR018','TK11','','Karina','Karina@gmail.co','81233445583'),('KR019','TK12','','Prabowo','Prabowo@gmail.c','81233445584'),('KR020','TK07','','Joko','Joko@gmail.com','81233445585'),('KR021','TK08','','Totok','Totok@gmail.com','81233445586'),('KR022','TK09','','Diah','Diah@gmail.com','81233445587'),('KR023','TK10','','Mega','Mega@gmail.com','81233445588'),('KR024','TK11','','Wati','Wati@gmail.com','81233445589'),('KR025','TK12','','Indah','Indah@gmail.com','81233445590'),('KR026','TK07','','Dwi','Dwi@gmail.com','81233445591'),('KR027','TK08','','Pertiwi','Pertiwi@gmail.c','81233445592'),('KR028','TK09','','Eka','Eka@gmail.com','81233445593'),('KR029','TK10','','Mahendra','Mahendra@gmail.','81233445594'),('KR030','TK11','','Suyitno','Suyitno@gmail.c','81233445595');
+/*!40000 ALTER TABLE `KARYAWAN` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-30 18:51:26
---
+++ Semester 3/Basis Data Lanjut/quiz_1/before_DB/admin_basisdata_PELANGGAN.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
+--
+-- Host: 172.17.0.2    Database: admin_basisdata
+-- ------------------------------------------------------
+-- Server version	5.6.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `PELANGGAN`
+--
+
+LOCK TABLES `PELANGGAN` WRITE;
+/*!40000 ALTER TABLE `PELANGGAN` DISABLE KEYS */;
+INSERT INTO `PELANGGAN` VALUES ('PL001','ijul','ijul24','ijul11','1999-02-02','L',81123456789,'ijul@gmail','jl denpasar no 5','Jakarta'),('PL002','yoga','yoga66','yogaa','1970-03-06','L',85987654321,'yoga@gmail.com','jl ijen no 1','Jakarta'),('PL003','rin','rinkin','rin123','1980-11-07','P',83678901234,'rin@yahoo.com','jl singaraja no 9','Jakarta'),('PL004','andi','andiii','andibuz','1998-05-20','L',85123459876,'andi@yahoo.com','jl bromo no 33','Jakarta'),('PL005','setiawan','wawan','setiawan16','2000-01-03','L',81678943210,'setiawan88@gmail.com','jl candi no 55','Jakarta'),('PL006','aziz','aziz77','aziz10','1998-02-09','L',87124361567,'aziz@Gmail.com','jl semeru no 99','Malang'),('PL007','belinda','belbel','belinda98','1997-07-06','P',81678943210,'belinda6@yahoo.co.id','jl prabu no 11','Malang'),('PL008','juminten','jumintennn','jumjum998','1999-06-18','P',85987654321,'jumjum@Gmail.com','jl galunggung no 97','Malang'),('PL009','gilbert','gill','gilbert','1997-05-09','L',81123456789,'gill@yahoo.com','jl bandung no 44','Malang'),('PL010','axel','axe','axeblood','1987-07-03','L',85987654321,'axe@gmail.com','jl kemerdekaan no 9','Malang'),('PL011','ibnu','ibnuu','ib12','1978-05-03','L',81678943210,'ibnu@Gmail.com','jl prabu no 12','Surabaya'),('PL012','masrud','masrud11','masrud17','1998-06-17','L',83678901234,'masrud@Gmail.com','jl denpasar no 7','Surabaya'),('PL013','gilang','gilanggg','gilll12','1998-07-03','L',81123456789,'gilang@yahoo.com','jl denpasar no 10','Surabaya'),('PL014','fery','ferdinan','ferr7','1990-04-03','L',81678943210,'fey@Gmail.com','jl prabu no 11','Surabaya'),('PL015','wawab','waaa','w99','1998-05-01','L',81123456789,'wawan@Gmail.com','jl semangka no 99','Surabaya'),('PL016','paujul','pujul98','punjul66','1899-12-31','L',82144462645,'paujul@yahoo.com','jl gurami no 03','Solo'),('PL017','medik','mediksku','Medik098','1900-02-24','L',85646312646,'medik98@Gmail.com','jl ikankakap no 15','Solo'),('PL018','prenk','prenklin','Prenklin77','1950-01-11','L',81234334379,'prenklin@yahoo.com','jl bakso no 24','Solo'),('PL019','temon','temon90','Montemon2','1997-09-26','L',89536489654,'temon2@Gmail','jl dinaya no 6','Solo'),('PL020','lambskay','lambe','lambemu33','2005-06-09','L',81753695421,'lambee33@Gmail.com','jl wage no 13','Solo'),('PL021','makki','makki21','makleren56','1986-04-15','P',87989464131,'makleren09@yahoo.com','jl sumber no 5','Yogyakarta'),('PL022','doyok','wakdoyok','Doyok343','1992-05-27','L',89864215348,'wadoyok@Gmail.com','jl H.omo no 8','Yogyakarta'),('PL023','apep','apeptelle','Apeple001','1989-06-16','L',81328745622,'apep01@Gmail','jl kalimaya no18','Yogyakarta'),('PL024','mbonsky','mbontole','Tolle76','1996-06-06','L',85213647964,'mbonsky@yahoo.com','jl gatot no 20','Yogyakarta'),('PL025','fadhil','fadheli','fadelay81','1990-01-24','L',82165445511,'fadheli81@Gmail.com','jl trikora no 4','Yogyakarta'),('PL026','ria','riacantik','Riacan59','2000-03-07','P',89678829361,'ria@Gmail','jl tagulamgin no 14','Bandung'),('PL027','dodotasoy','dodot','dodotasoygeboy','1985-08-16','L',87992277883,'dodot54@Gmail.com','jl somba opu no 35','Bandung'),('PL028','icun','icun44','icuna39','1994-07-30','P',85631002163,'icuna@yahoo.com','jl gang lombok no 52','Bandung'),('PL029','reo','reareo','Reneo646','1992-02-02','L',81477003214,'reo6646@Gmail.com','jl cihampelas no 87','Bandung'),('PL030','junet','junaidi','Junett34','1981-12-12','L',87001003009,'junaidi@yahoo.com','jl tujungan no 100','Bandung');
+/*!40000 ALTER TABLE `PELANGGAN` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-30 18:32:41
