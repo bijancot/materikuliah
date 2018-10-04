db7d20873c907af54c916adfc4dd78260a8b273f
bijancot <panjidia995@gmail.com>
2018-10-04 07:43:11 +0700

    fix addbukualamat

--- Semester 3/Praktikum Pemprograman Dasar 2/Pertemuan 2/bukualamat.java
+++ Semester 3/Praktikum Pemprograman Dasar 2/Pertemuan 2/bukualamat.java
@@ -18,14 +18,14 @@ public class bukualamat{
         alamat.add(new addbukualamat("Panji","GPA"));
         data.put("no1",alamat);
         alamat = new ArrayList<addbukualamat>();
-        alamat.add(new addbukualamat("Panji2","GPA"));
+        alamat.add(new addbukualamat("Panji2","GPA2"));
         data.put("no2",alamat);
         alamat = new ArrayList<addbukualamat>();
-        alamat.add(new addbukualamat("Panji3","GPA"));
+        alamat.add(new addbukualamat("Panji3","GPA3"));
         data.put("no3",alamat);
         
         for(int i =0;i<data.size();i++){
-            System.out.println(data.get("no"+(i+1)).get(0).nama);
+            System.out.println(data.get("no"+(i+1)).get(0).nama + data.get("no"+(i+1)).get(0).alamat);
         }
 
 
