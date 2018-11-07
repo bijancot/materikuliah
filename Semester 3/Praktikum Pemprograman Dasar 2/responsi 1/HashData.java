762abe7ec75d6fa33215cb4439b38ff7b56cb4c4
bijancot <panjidia995@gmail.com>
2018-11-07 14:55:01 +0700

    add responsi 1 PDII

---
+++ Semester 3/Praktikum Pemprograman Dasar 2/responsi 1/HashData.java
@@ -0,0 +1,63 @@
+//@author bijancot 
+//github = https://github.com/bijancot
+
+import java.util.Hashtable;
+import java.util.Random;
+import java.util.ArrayList;
+import java.math.*;
+
+class addhashdata{
+String soal,jawaban;
+    addhashdata(String b, String c){
+        soal = b;
+        jawaban = c;
+    }
+}
+public class HashData{
+    static Hashtable<String,ArrayList<addhashdata>> proses(){
+        Hashtable<String,ArrayList<addhashdata>>  data =  new Hashtable<String,ArrayList<addhashdata>>();
+        ArrayList<addhashdata> AofD = new ArrayList<addhashdata>();
+        ArrayList<String> soal = new ArrayList<String>();
+        ArrayList<String> jawaban = new ArrayList<String>();
+
+        soal.add("Pantai Tempat Orang ? ");
+        soal.add("Hujan turun dari ? ");
+        soal.add("Burung Terbang Dengan ? ");
+        soal.add("Seekor Rusa Memiliki ? ");
+        soal.add("Burung adalah hewan yang bisa? ");
+        soal.add("Bendera NKRI warnanya ? ");
+        soal.add("Ketika ..... orang biasanya makan ?");
+        soal.add("Kenapa orang makan dengan tangan kanan?");
+        soal.add("Apakah yang dilakukan orang sakit ?");
+        soal.add("yang sering mendapat nilai 100 saat ulangan adalah?  ");
+
+        jawaban.add("ramai");
+        jawaban.add("maghrib");
+        jawaban.add("semaunya");
+        jawaban.add("temann");
+        jawaban.add("temenan");
+        jawaban.add("cuma dua aja");
+        jawaban.add("marah");
+        jawaban.add("karena makanan");
+        jawaban.add("pulang");
+        jawaban.add("kertas");
+
+
+        for(int i = 0; i < jawaban.size(); i++){
+            AofD = new ArrayList<addhashdata>();
+            AofD.add(new addhashdata(soal.get(i), jawaban.get(i)));
+            data.put(("S"+i),AofD);   
+        }
+        return data;
+    }
+
+    static ArrayList<String> random(){
+        ArrayList<String> dato = new ArrayList<String>();
+        for(int x=0; x<6;x++){
+            Random a = new Random();
+            int y = a.nextInt(10);
+            dato.add("S"+y);
+        }
+        return dato;
+    }
+}
---
+++ Semester 3/Praktikum Pemprograman Dasar 2/responsi 1/MainApp.java
@@ -0,0 +1,58 @@
+//@author bijancot 
+//github = https://github.com/bijancot
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class MainApp{
+    public static void main(String args[]){
+        Scanner r = new Scanner(System.in);
+        ArrayList<String> a = new ArrayList<String>();
+        Hashtable<String,ArrayList<addhashdata>> y = new Hashtable<String,ArrayList<addhashdata>>();
+        a = HashData.random();
+        y = HashData.proses();
+
+        System.out.println("===========================================");
+        System.out.println("Yuk, Main Yuk!!");
+        System.out.println("===========================================");
+        System.out.println("\n\n");
+        System.out.println("akan ada 6 soal acak tolong \njawab dengan baik ya!! \nsetiap soal ada dua kali \nkesempatan menjawab");
+        System.out.println("===========================================");
+        System.out.println("===========================================");
+            int p = a.size()-1;
+            for(int t=0;t<=p;t++){
+                int z =2;
+                while (z!=0){
+                System.out.println("Soal"+(t+1)+": ");
+                System.out.println("------------------------");
+                System.out.println(y.get(a.get(t)).get(0).soal);
+                System.out.println("Jawaban anda : ");
+                String jwb = r.nextLine();
+                if(jwb.trim().equals(y.get(a.get(t)).get(0).jawaban)==true){
+                    System.out.println("------------------------");
+                    System.out.println("Jawaban Benar");
+                    System.out.println("------------------------\n\n");
+                    z=0;
+                }
+                else if(jwb.trim().equals(y.get(a.get(t)).get(0).jawaban)==false){
+                    System.out.println("------------------------");
+                    System.out.println("Jawaban Salah");
+                    System.out.println("------------------------\n");
+                    z = z-1;
+                    System.out.println("Kesempatan mengulang : "+z);
+                    System.out.println("------------------------\n\n");
+                    if(z==0){
+                        System.out.println("------------------------");
+                        System.out.println("Jawaban benar adalah : "+y.get(a.get(t)).get(0).jawaban);
+                        System.out.println("------------------------");
+                    }
+                }
+                }
+            }
+            r.close();
+            System.out.println("celecai");
+            System.out.println("TERIMAKASIH");
+    }
+}
